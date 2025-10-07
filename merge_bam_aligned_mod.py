6c016038f0b3b05496c8c8dad99b191ed824d5b7
dongxujiang189-droid <dongxujiang189@gmail.com>
2025-10-07 15:52:24 -0400

    Update merge_bam_aligned_mod.py

--- merge_bam_aligned_mod.py
+++ merge_bam_aligned_mod.py
@@ -4,21 +4,23 @@ unaligned_mod_bam = "/mnt/e/Data/seq_for_human_293t2/barcode04/barcode04_merged.
 aligned_bam = "/mnt/e/Data/seq_for_human_293t2/barcode04/barcode04_aligned.sorted.bam"
 output_bam = "/mnt/e/Data/seq_for_human_293t2/barcode04/barcode04_aligned_with_mod.bam"
 
-# Open unaligned BAM with check_sq=False
 unaligned = pysam.AlignmentFile(unaligned_mod_bam, "rb", check_sq=False)
 aligned = pysam.AlignmentFile(aligned_bam, "rb")
 out = pysam.AlignmentFile(output_bam, "wb", template=aligned)
 
+# Build a dictionary of unaligned reads
+unaligned_dict = {}
+for u in unaligned:
+    unaligned_dict[u.query_name] = u
+
 # Merge MM/ML tags
 for a in aligned:
-    try:
-        u = next(unaligned.fetch(a.query_name))
+    u = unaligned_dict.get(a.query_name)
+    if u:
         if u.has_tag("MM"):
             a.set_tag("MM", u.get_tag("MM"))
         if u.has_tag("ML"):
             a.set_tag("ML", u.get_tag("ML"))
-    except StopIteration:
-        pass
     out.write(a)
 
 unaligned.close()
